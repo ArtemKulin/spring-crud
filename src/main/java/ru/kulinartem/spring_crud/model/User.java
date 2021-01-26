90cbd45df2243ddf3f11153a2236b576e9d95ed4
Artem Kulin <kulinartem@gmail.com>
2021-01-26 22:08:24 +0300

    add User model

---
+++ src/main/java/ru/kulinartem/spring_crud/model/User.java
@@ -0,0 +1,74 @@
+package ru.kulinartem.spring_crud.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "lastname")
+    private String lastName;
+
+    @Column (name = "age")
+    private int age;
+
+    @Column (name = "gender")
+    private char gender;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, int age, char gender) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
