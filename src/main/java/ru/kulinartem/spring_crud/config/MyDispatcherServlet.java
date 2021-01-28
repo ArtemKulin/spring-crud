56a96dd35608c11dded004a2dbce99f04f3dd6ab
Artem Kulin <kulinartem@gmail.com>
2021-01-28 15:24:58 +0300

    add http Filter in MyDispatcherServlet config file

--- src/main/java/ru/kulinartem/spring_crud/config/MyDispatcherServlet.java
+++ src/main/java/ru/kulinartem/spring_crud/config/MyDispatcherServlet.java
@@ -1,6 +1,10 @@
 package ru.kulinartem.spring_crud.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MyDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,16 @@ public class MyDispatcherServlet extends AbstractAnnotationConfigDispatcherServl
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext context) {
+        context.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
